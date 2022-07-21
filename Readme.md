85afbd4f072063b10fa9b591e7320f38900e7f06
diego-lucas-silva <silva.diegolucas@gmail.com>
2022-07-21 15:15:02 -0300

    Projeto inicial completo e funcionando, com ideias de melhorias a serem implementadas

--- Readme.md
+++ Readme.md
@@ -1,6 +1,11 @@
 20/07/2022
-Construindo uma pokedex que pesquisa o nome ou o numero de pokemon.
+Construindo uma pokedex que pesquisa por nome ou por numero os pokemons.
 
-- Implementações:
-  [ ] front
-  [ ] back com api que consome de outro site
+- Implementações feitas:
+  [x] front -> html e css simples.
+  [x] back -> utilizando JS, com api que consome de site externo.
+
+- melhorias
+  [] melhorar a parte do front, colocando como se fosse uma pokedex mesmo.
+  [] criar um jogo onde aparece o pokemon e tem que escreve o nome corretamente.
+  [] criar um jogo que aparece três pokemons diferentes e uma pergunta sobre algo desses três pokemons. Por exemplo, aparece três pokemons e a pergunta é qual deles é o pikachu, sendo que um desses três é o pikachu.
--- script.js
+++ script.js
@@ -3,7 +3,56 @@ var formulario = document.querySelector('form')
 formulario.addEventListener('submit', function (e) {
   // bloqueia o refresh automático da página
   e.preventDefault()
+
   // url da pesquisa
   let urlForm = 'https://pokeapi.co/api/v2/pokemon/'
+
   //valor do input name
+  let nome = document.getElementById('name')
+
+  //Concatena a url com input name
+  urlForm = urlForm + this.name.value
+
+  // transforma os valores em minúsculas
+  urlForm = urlForm.toLocaleLowerCase()
+
+  // ID contents
+  let resposta = document.getElementById('content')
+
+  //ID imgPokemon
+  let imagem = document.getElementById('imgPokemon')
+
+  // resposta em HTML
+  let html = ''
+
+  //puxando os dados
+  fetch(urlForm)
+    .then(resposta => resposta.json())
+    .then(function (data) {
+      console.log(data)
+      html = 'Nome: ' + maiuscula(data.name) + '<br>'
+      html = html + 'Type: ' + maiuscula(data.types[0].type.name)
+      resposta.innerHTML = html
+
+      imagem.innerHTML =
+        "<img src='" +
+        data.sprites.front_default +
+        "'><img src='" +
+        data.sprites.back_default +
+        "'>"
+    })
+
+    //tratamento de erro
+    .catch(function (err) {
+      if (err == 'SyntaxError: Unexpected token N in JSON at position 0') {
+        html = 'Pokemon não encontrado 😢'
+      } else {
+        html = 'Erro:' + err
+      }
+      resposta.innerHTML = html
+    })
 })
+
+function maiuscula(val) {
+  return val[0].toUpperCase() + val.substr(1)
+}
