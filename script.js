378537db9949469f066a527e243f267ef1d7067a
diego-lucas-silva <silva.diegolucas@gmail.com>
2022-07-21 12:56:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', function (e) {
+  // bloqueia o refresh automático da página
+  e.preventDefault()
+  // url da pesquisa
+  let urlForm = 'https://pokeapi.co/api/v2/pokemon/'
+  //valor do input name
+})
